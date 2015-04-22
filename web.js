7afd9cefce0100e7b55009dda375216f48ec517a
Paul Kassianik <pkassianik@gmail.com>
2015-04-21 20:44:49 -0700

    changed port

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 80));
 app.get('/', function(req, res) {
     res.send('The Right Thing!');
 });
-app.listen(3000);
+app.listen(app.get('port'));
