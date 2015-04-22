0685b949ad08dcda04415c5870d6fe640ade4508
Paul Kassianik <pkassianik@gmail.com>
2015-04-21 21:50:39 -0700

    trying to change

--- web.js
+++ web.js
@@ -4,26 +4,17 @@ var app = express();
 app.set('port', (process.env.PORT || 80));
 console.log(fs.readFile("./main_page.html"));
 app.get('/', function(req, res) {
-    res.send("<html><head><title>Some Title</title></head><body><script> window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1603688843233149',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
+    var string = "<html><head><title>Some Title</title></head><body>";
+    string += "<script> window.fbAsyncInit = function() { FB.init({ appId      : '1603688843233149', xfbml      : true,  version    : 'v2.3'  }) };"
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
+   string += "(function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = '//connect.facebook.net/en_US/sdk.js';
+     js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-</script><div
-  class='fb-like'
-  data-share='true'
-  data-width='450'
-  data-show-faces='true'>
-</div></body></html>");
+</script>" ;
+    string += "<h1>HI</h1></body></html>"
+    res.send(string);
 });
 app.listen(app.get('port'));
