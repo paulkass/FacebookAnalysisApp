afa67795b486870552d897bd33a1e919ade876b1
Paul Kassianik <pkassianik@gmail.com>
2015-04-21 20:51:13 -0700

    added html file

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.set('port', (process.env.PORT || 80));
 app.get('/', function(req, res) {
-    res.send('The Right Thing!');
+    res.send(fs.readFile("./main_page.html");
 });
 app.listen(app.get('port'));
