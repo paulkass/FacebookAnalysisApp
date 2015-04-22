8e141c2545a0792645d3c5ef6b63ab3d684da85a
Paul Kassianik <pkassianik@gmail.com>
2015-04-21 21:55:17 -0700

    trying to change

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
     string += "<script> window.fbAsyncInit = function() { FB.init({ appId      : '1603688843233149', xfbml      : true,  version    : 'v2.3'  }) };"
 
    string += "(function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = '//connect.facebook.net/en_US/sdk.js'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk')); </script>" ;
-    string += "<h1>HI</h1></body></html>"
+    string += "<div class='fb-like' data-share='true' data-width='450' data-show-faces='true'></div><h1>HI</h1></body></html>"
     res.send(string);
 });
 app.listen(app.get('port'));
