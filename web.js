bc60eee47802bbfd2c5326b5958050162f3f428d
Paul Kassianik <pkassianik@gmail.com>
2015-04-21 21:51:59 -0700

    trying to change

--- web.js
+++ web.js
@@ -7,13 +7,7 @@ app.get('/', function(req, res) {
     var string = "<html><head><title>Some Title</title></head><body>";
     string += "<script> window.fbAsyncInit = function() { FB.init({ appId      : '1603688843233149', xfbml      : true,  version    : 'v2.3'  }) };"
 
-   string += "(function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>" ;
+   string += "(function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = '//connect.facebook.net/en_US/sdk.js'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk')); </script>" ;
     string += "<h1>HI</h1></body></html>"
     res.send(string);
 });
