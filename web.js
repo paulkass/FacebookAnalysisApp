0d7b2c27f7f55afc0f68604355d816991091e50b
Paul Kassianik <pkassianik@gmail.com>
2015-04-21 21:07:17 -0700

    trying to change

--- web.js
+++ web.js
@@ -3,6 +3,11 @@ var fs = require('fs');
 var app = express();
 app.set('port', (process.env.PORT || 80));
 app.get('/', function(req, res) {
-    res.send(fs.readFile("./main_page.html"));
+    res.send(fs.readFile("./main_page.html"), function(err) {
+	if (err) {
+	  console.log(err);
+	}
+	console.log(data);
+	});
 });
 app.listen(app.get('port'));
